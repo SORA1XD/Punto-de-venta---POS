22cf74663e7ba7df6cef93d6e7f74efd43ea5de4
Manuel Bustamante Navarro <manuel.bustamanten@outlook.com>
2020-11-25 16:12:11 -0300

    .

---
+++ Capa datos/Class_Categorias.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Capa_datos
+{
+    class Class_Categorias
+    {
+        private int _cat_cod;
+        private string _cat_nombre;
+        private string _cat_descripcion;
+
+    }
+}
